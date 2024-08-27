e76715b208bab614f663b3e462277a610c9458a8
surendranadhilla <surendranadhilla@gmail.com>
2024-08-27 16:30:48 -0500

    first commit

---
+++ ArithmeticOperations.java
@@ -0,0 +1,16 @@
+package com.test;
+
+public class ArithmeticOperations {
+
+	public static void main(String[] args) {
+
+		int a, b, c;
+
+		a = 10;
+		b = 20;
+		c = a + b;
+		System.out.println("Sum of two numbers c = " + c);
+
+	}
+
+}
